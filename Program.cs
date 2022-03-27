e412e390bce09daeca1c19554d5ee5ff6fe7232d
Siddhant <santoshshrivastava39@gmail.com>
2022-03-28 00:17:37 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace RegexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name");
+            string FirstName = Console.ReadLine();
+            Pattern pattern = new Pattern();
+
+            if (pattern.Validatefirstname(FirstName))
+            {
+                Console.WriteLine("Name matched");
+            }
+            else if (FirstName.Length < 3)
+            {
+                Console.WriteLine("Please enter atleast three characters");
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Name does not matched");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
