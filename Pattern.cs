ce4a583e935ecf84cb15317ac4f8c75b60b4ebfe
Siddhant <santoshshrivastava39@gmail.com>
2022-03-28 00:19:08 +0530

    Valid First Name

---
+++ Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class Pattern
+    {
+        public static string RegexPattern = "^[A-Z]{1}[A-Z a-z]{7}$";
+
+        public bool Validatefirstname(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, RegexPattern);
+        }
+    }
+}
