b52ab6d96563be57d08ebe249a7510d6675c8df8
Siddhant <santoshshrivastava39@gmail.com>
2022-03-28 14:46:13 +0530

    Enter Valid LAST Name

--- Pattern.cs
+++ Pattern.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 using System.Text;
 using System.Text.RegularExpressions;
 
+
 namespace RegexDemo
 {
     public class Pattern
     {
-        public static string RegexPattern = "^[A-Z]{1}[A-Z a-z]{7}$";
-
-        public bool Validatefirstname(string FirstName)
+        public static string RegexPattern = "^[A-Z]{1}[A-Z a-z]{2,}$";
+     
+        public bool ValidateLastname(string LastName)
         {
-            return Regex.IsMatch(FirstName, RegexPattern);
+            return Regex.IsMatch(LastName, RegexPattern);
         }
-    }
+    } 
 }
